b1798d1685483141cb0852c11b4d55b50aaa9686
Suganya <suganprakash0621@gmail.com>
2020-10-19 15:49:10 -0500

    modified some changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,14 @@ import {CriminalList} from "./criminal/criminallist.js"
 import {getCriminals} from "./criminal/criminaldataprovider.js"
 import {getConvictions} from "./convictions/ConvictionProvider.js"
 import {ConvictionSelect} from "./convictions/ConvictionSelect.js"
+import {getOfficers} from"./officers/OfficerProvider.js"
+import {Officerlist} from "./officers/officerslist.js"
 getCriminals();
 CriminalList();
 getConvictions();
 ConvictionSelect();
-
+getOfficers();
+Officerlist();
 
 
 
