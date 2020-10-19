e056dbea737d5a49bf2dfc878ce0519f54b65776
Suganya <suganprakash0621@gmail.com>
2020-10-19 13:38:40 -0500

    Coded for Convictionprovider.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import {CriminalList} from "./criminal/criminallist.js"
 import {getCriminals} from "./criminal/criminaldataprovider.js"
+import {getConvictions} from "./convictions/ConvictionProvider.js"
 getCriminals();
 CriminalList();
+getConvictions();
 
 
 
