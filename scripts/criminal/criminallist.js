b1c4447b09ae4e29fdb272d48d14cfd2c9742440
Suganya <suganprakash0621@gmail.com>
2020-10-20 23:24:18 -0500

    done coding for criminallist & convictionprovider

--- scripts/criminal/criminallist.js
+++ scripts/criminal/criminallist.js
@@ -1,23 +1,49 @@
 import { getCriminals, useCriminals } from './criminaldataprovider.js'
 import {criminalcard} from "./criminal.js"
+import {useConvictions} from "../convictions/ConvictionProvider.js"
 
 const contentElement = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
+ console.log(eventHub)
 export const CriminalList = () => {
     getCriminals().then( () => { 
         const criminals = useCriminals();
-    let criminalHTML = ""
-for(const numbers of criminals){
-    criminalHTML += criminalcard(numbers)
-
-    contentElement.innerHTML =  
-`<h3>  Glassdale Criminals   </h3>
-<section  class="criminals" > 
-${criminalHTML}
-</section>`
-
-}
+    
     
 })
 }
 
+eventHub.addEventListener("crimeSelected", event => {
+        console.log("crimeSelected",event.detail.crimeThatWasChosen)
+        if(event.detail.crimeThatWasChosen !== 0) { 
+        const criminals = useCriminals()
+        console.log(criminals)
+        const convictions = useConvictions()
+         console.log("convictions",convictions)
+        const convictionThatWasChosen = convictions.find(convictionObj => {
+        return convictionObj.id === event.detail.crimeThatWasChosen
+        })
+        console.log("convictionThatWasChosen",convictionThatWasChosen)
+   
+        const filteredArray = criminals.filter(criminalObj =>{
+            return criminalObj.name === convictionThatWasChosen.name 
+        })
+      console.log("filtered",filteredArray)
+      render (filteredArray)
+    }
+})
+  
+      const render = () => { 
+      let criminalHTML = ""
+    for(const numbers of filteredArray){
+    criminalHTML += criminalcard(numbers)
+
+    contentElement.innerHTML =  
+    `<h3>  Glassdale Criminals   </h3>
+    <section  class="criminals" > 
+    ${criminalHTML}
+    </section>`
+
+        }
+    }
 
