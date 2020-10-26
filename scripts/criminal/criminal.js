347a50671af121458b8907248755d93791bcf657
Suganya <suganprakash0621@gmail.com>
2020-10-26 13:10:37 -0500

    all codes dones got crimina;.js

--- scripts/criminal/criminal.js
+++ scripts/criminal/criminal.js
@@ -1,3 +1,5 @@
+const eventHub = document.querySelector(".container")
+
 export const criminalcard =(criminalObj) => {
     return `
     <section class="criminals" >
@@ -7,8 +9,21 @@ export const criminalcard =(criminalObj) => {
     <p class= "criminals__crime">Crime: ${criminalObj.conviction} </p>
     <p class= "criminals__start">Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
     <p class= "criminals__end">Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
-    <button id="associates--${criminal.id}">Associate Alibis</button>
+    <button id="associates--${criminalObj.id}">Associate Alibis</button>
     </section>
     `
 }
+eventHub.addEventListener("click",clickEvent =>{
+const[prefix, criminalId] = clickEvent.target.id.split("--")
+console.log(prefix, criminalId)
 
+if( clickEvent.target.id.startsWith("associates--")){
+    const myCustomEvent = new CustomEvent("alibibuttonClicked",{
+        detail:{
+            criminal:criminalId
+        }
+    })
+        eventHub.dispatchEvent(myCustomEvent)
+
+}
+})
